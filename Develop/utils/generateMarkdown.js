8169bbb7bd448f812d172b0cea2b0e92baa04822
Kiahna Carmack <Kiahna.carmack@gmail.com>
2021-11-21 13:55:24 -0800

    commit main

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return renderLicenseBadge(data)
+  console.error("")
+  
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -10,8 +14,8 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  try {error} finally {
+ const renderLicenseSection= license => {
+try {error} finally {
        console.error("");
         console.log(err);
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # readMe-Generator
+WHEN the readME generator is used, the user should be prompted to enter information regarding their project.
+THEN once the prompts have been submitted, the terminal will create a detailed format with a read me 
